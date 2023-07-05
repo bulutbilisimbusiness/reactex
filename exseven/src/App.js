a8bbf735db8551928a7e3786ddd2c33c307613b4
bulutbilisimbussiness <bulutbilisimbussiness@gmail.com>
2023-07-05 23:13:54 +0300

    useState

--- exseven/src/App.js
+++ exseven/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import Events from "./events/Events";
+//import Counter from "./classComponent/Counter";
+//import UseStateCounter from "./useState/useStateCounter";
+import Spread from "./useState/Spread";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Spread />
+      {/* <Events />
+      <Counter />
+      <UseStateCounter /> */}
     </div>
   );
 }
-
 export default App;
---
+++ exseven/src/useState/Spread.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState } from "react";
+
+const Spread = () => {
+  /* const [name, setName] = useState("Ahmet Can");
+  const [age, setAge] = useState(30);
+  const [salary, setSalary] = useState(50000); */
+  const [kisi, setKisi] = useState({
+    name: "Ahmet Can",
+    age: 30,
+    salary: 50000,
+  });
+  const [toggle, setToggle] = useState(true);
+  //let toggle = false;
+  const handleToggle = () => {
+    //toggle = !toggle;
+    setToggle(!toggle);
+    if (toggle) {
+      setKisi({
+        name: "Aliye Cankaya ",
+        age: 48,
+        salary: 67900,
+      });
+    } else {
+      setKisi({
+        name: "Ahmet Can",
+        age: 30,
+        salary: 50000,
+      });
+    }
+  };
+  return (
+    <div>
+      <h1>USE STATE OBJECT</h1>
+
+      {/* <h2>NAME:{name}</h2>
+
+      <h2>AGE:{age}</h2>
+      <h2>SALARY:{salary}</h2>
+      <button onClick={() => setName("Erhan Bulut")}>Change Name</button>
+      <button onClick={() => setAge(age + 1)}>inc age</button>
+      <button onClick={() => setSalary(salary + 10000)}>inc salary</button> */}
+      <h2>NAME:{kisi.name}</h2>
+
+      <h2>AGE:{kisi.age}</h2>
+      <h2>SALARY:{kisi.salary}</h2>
+      <button onClick={() => setKisi({ ...kisi, name: "Erhan Bulut" })}>
+        Change Name
+      </button>
+      <button onClick={() => setKisi({ ...kisi, age: kisi.age + 1 })}>
+        inc age
+      </button>
+
+      <button onClick={() => setKisi({ ...kisi, salary: kisi.salary + 1000 })}>
+        inc age
+      </button>
+      <button
+        onClick={() =>
+          setKisi({
+            name: prompt("Enter new name"),
+            age: parseInt(prompt("Enter new Age")),
+            salary: parseFloat(prompt("Enter New Salary")),
+          })
+        }
+      >
+        Full Change
+      </button>
+      <button onClick={handleToggle}>Toggle</button>
+    </div>
+  );
+};
+export default Spread;
