c1a199ef4cee6ad4e453538a59112bd21e7b3b3f
bulutbilisimbussiness <bulutbilisimbussiness@gmail.com>
2023-07-19 21:03:23 +0300

    router

---
+++ exeleven/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <ul className="bg-warning">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <NavLink to="/instructors">Instructors</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default Nav;
