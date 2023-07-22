b58cb718b790be736e2dfb886b62e8b89e394e98
bulutbilisimbussiness <bulutbilisimbussiness@gmail.com>
2023-07-22 14:30:43 +0300

    contex-reducer

---
+++ ex14/src/pages/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { useLoginContext } from "../context/LoginProvider";
+
+const PrivateRouter = () => {
+  const { user } = useLoginContext();
+  return user?.email && user?.password ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
